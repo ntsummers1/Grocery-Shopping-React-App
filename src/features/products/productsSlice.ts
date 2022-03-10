d69ac9e9264af7ff608789fd4c18f6e94c779616
Nicholas Summers <ntsummers1@gmail.com>
2022-03-10 16:37:18 -0500

    Pass productsSlice test and output to page

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import axios from 'axios';
+import { RootState } from '../../app/store';
 import IProductsFilter from './productsFilterInterface';
 import IProducts from './productsInterface';
 
@@ -16,18 +17,51 @@ const initialState: IProductsState = {
 }
 
 export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
-
+  const endpoint = "../../endpointResponses/products.json"
+  console.log(endpoint)
+  if (endpoint) {
+    const res = await axios.get(endpoint)
+    return res.data
+  } else {
+    return Promise.reject("Unable to connect to endpoint")
+  }
 })
 
 const productsSlice = createSlice({
   name: 'Products Reducer',
   initialState,
   reducers: {
-    filterProducts: (state, action: PayloadAction<IProductsFilter>) => {}
+    filterProducts: (state, action: PayloadAction<IProductsFilter>) => {
+      const filters = action.payload;
+      state.filteredProducts = state.products
+      if (filters.instock) {
+        state.filteredProducts = state.filteredProducts.filter(item => item.instock === filters.instock )
+      }
+      if (filters.name) {
+        state.filteredProducts = state.filteredProducts.filter(item => item.name.includes(filters.name))
+      }
+    }
   },
-  extraReducers: {}
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'idle'
+        state.products = action.payload
+        state.filteredProducts = action.payload
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.status = 'error'
+      })
+  }
 });
 
 export const {filterProducts} = productsSlice.actions
+
+export const selectFilteredProducts = (state: RootState) => state.products.filteredProducts
+export const selectProducts = (state: RootState) => state.products.products
+
 
 export default productsSlice.reducer
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import Products from "../features/products/Products";
+import {
+  fetchProducts,
+  selectFilteredProducts,
+} from "../features/products/productsSlice";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const dispatch = useAppDispatch();
+  const selectProducts = useAppSelector(selectFilteredProducts);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      HomePage
+      <Products products={selectProducts} />
+    </div>
+  );
 };
 
 export default HomePage;
