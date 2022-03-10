bbaaa11b752eeeaffaca1a1775dd3ff76d9a9183
Nicholas Summers <ntsummers1@gmail.com>
2022-03-10 16:00:26 -0500

    Hookup React-Router-Dom and Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CartPage from "./pages/CartPage";
+import FourOhFour from "./pages/FourOhFour";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProductsPage from "./pages/ProductsPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="*" element={<FourOhFour />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
