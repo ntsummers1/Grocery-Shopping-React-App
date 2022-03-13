4c1c50f5dbefc682a2add31920bae7dc19e47700
Nicholas Summers <ntsummers1@gmail.com>
2022-03-13 12:10:21 -0400

    Create basic layout styling

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Product from "./product/Product";
 import IProducts from "./productsInterface";
+import "./products.css";
 
 type Props = {
   products: IProducts;
@@ -9,7 +10,8 @@ type Props = {
 const Products = ({ products }: Props) => {
   return (
     <>
-      <ul>
+      <h1 className="products-title">Products</h1>
+      <ul className="products">
         {products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
--- src/features/products/product/Product.tsx
+++ src/features/products/product/Product.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import IProduct from "./productInterface";
+import "./product.css";
 
 type Props = {
   product: IProduct;
 };
 
 const Product = ({ product }: Props) => {
-  return <li>{product.name}</li>;
+  return (
+    <li className="product">
+      <img src={product.img} alt={product.name} className="product-img" />
+      <p className="product-name"> {product.name} </p>
+      <div className="product-info-row">
+        <p className="product-price">${product.price}</p>
+        <button className="product-add" disabled={!product.instock}>
+          +
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default Product;
