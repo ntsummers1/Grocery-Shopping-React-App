0f11619bd489ab390b72976118b09c2e3832088c
Nick Summers <ntsummers1@gmail.com>
2022-04-17 12:42:49 -0400

    Add icons package and setup header to handle different layouts

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { BsBasket } from 'react-icons/bs'
+import { Link, useLocation } from 'react-router-dom'
 import { useAppSelector } from '../../app/hooks'
 import { selectCartItemsLength } from '../cart/cartSlice'
+
+const renderNavBasedOnUrl = (location : string, amountOfItems: number) => {
+    switch (location) {
+        case "/cart":
+            return <>
+                <Link to={'/cart'}>
+                    <div>
+                        <BsBasket />
+                    </div>
+                </Link>
+                <Link to={'/cart'}>Basket</Link>
+
+                <h1> My Cart </h1>
+
+                <Link to={'/'}>X</Link>
+            </>
+    
+        default:
+            return <>
+                <Link to={'/cart'}>Cart {amountOfItems}</Link>
+                <Link to={'/'}>Products </Link>
+            </>
+    }
+}
 
 const Header = () => {
 
     const amountOfItems = useAppSelector(selectCartItemsLength)
 
+    const location = useLocation();
+
     return (
         <div>
-            <Link to={'/cart'}>Cart {amountOfItems}</Link>
-            <Link to={'/'}>Products </Link>
+            { renderNavBasedOnUrl(location.pathname, amountOfItems) }
         </div>
     )
 }
