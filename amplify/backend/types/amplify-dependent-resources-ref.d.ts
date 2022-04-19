eb8309be9e6883c8821cd03ce1fe5605e74dfe10
Nick Summers <ntsummers1@gmail.com>
2022-04-19 14:46:13 -0400

    Implement Amplify/Cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "groceryshoppingreacta3fa8198": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
